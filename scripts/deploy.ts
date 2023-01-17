8753a10c92d6279f8d3fa29b72edb629f0bae2f2
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2023-01-17 19:32:04 +0530

    created scripts for the contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,9 +8,11 @@ const main = async () => {
 
   let txn = await nftContract.makeAnEpicNFT()
   await txn.wait()
+  console.log("Minted NFT #1")
   
   txn = await nftContract.makeAnEpicNFT()
   await txn.wait()
+  console.log("Minted NFT #2")
 };
 
 main().catch((error) => {
